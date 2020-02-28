068e99f27e459e1ee0d65c9e3d78eb3d5e4cdfc2
johann-miller <johann.miller@protonmail.com>
2020-02-28 15:37:06 -0500

    Adding basic html elements for home page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,10 @@
 <script>
-
+	let subjects = [
+		{
+			title: "Subject 1",
+			image: "https://via.placeholder.com/200x80"
+		},
+	]
 </script>
 
 <style>
@@ -11,9 +16,31 @@
 		border-bottom: 1px #d6d9dc solid;
 		margin-bottom: 1rem;
 	}
+
+	.subjects {
+		display: flex;
+		flex-flow: column;
+		list-style-type: none;
+	}
+
+	.subject {
+		border: 1px #d6d9dc solid;
+		margin-bottom: 1.5rem;
+		width: 20rem;
+		max-width: 100%;
+	}
 </style>
 
 <header>
 	<h1>Johann Miller / Blog</h1>
-	<p>A collection of my posts to help in studying mathematics</p>
+	<p>A collection of my posts to help you learn mathematics</p>
 </header>
+
+<ul class="subjects">
+	{#each subjects as subject}
+		<li class="subject">
+			<img src="{subject.image}" alt="subject-image">
+			<h2 class="subject-title">{subject.title}</h2>
+		</li>
+	{/each}
+</ul>
