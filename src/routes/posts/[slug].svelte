78e239c0fb2402bb39dcce4ce35573946f16b1b4
johann-miller <johann.miller@protonmail.com>
2020-04-30 17:04:19 -0400

    Fixed issue where switching between articles would not rerender math

--- src/routes/posts/[slug].svelte
+++ src/routes/posts/[slug].svelte
@@ -1,134 +1,141 @@
 <script>
-    import { stores } from "@sapper/app"
-    import { onMount, afterUpdate } from 'svelte'
-    import Post from '../../components/Post.svelte'
+  import { stores } from "@sapper/app";
+  import { onMount, afterUpdate } from "svelte";
+  import Post from "../../components/Post.svelte";
 
-    let { page } = stores()
-    let params = $page.params
-    let post
+  let { page } = stores();
+  let params = $page.params;
+  let post;
 
-    page.subscribe(value => {
-        params = value.params
-        updatePost(params.slug)
-    })
+  page.subscribe(value => {
+    params = value.params;
+    updatePost(params.slug);
+  });
 
-    onMount(() => {
-        console.log(params.slug)
-        updatePost(params.slug)
-    })
-
-    // Rerender any math after the post content has been updated
-    afterUpdate(() => {
-        if (window.MathJax) {
-            window.MathJax.Hub.Config({
-                tex2jax: {
-                    inlineMath: [["$", "$"], ["(", ")"]],
-                    displayMath: [["$$", "$$"], ["[", "]"]],
-                    processEscapes: true,
-                    processEnvironments: true
-                },
-                displayAlign: "center",
-                "HTML-CSS": {
-                    styles: { ".MathJax_Display": { margin: 0 } },
-                    linebreaks: { automatic: true }
-                }
-            });
+  onMount(() => {
+    if (window.MathJax) {
+      window.MathJax.Hub.Config({
+        tex2jax: {
+          inlineMath: [["$", "$"], ["(", ")"]],
+          displayMath: [["$$", "$$"], ["[", "]"]],
+          processEscapes: true,
+          processEnvironments: true
+        },
+        displayAlign: "center",
+        "HTML-CSS": {
+          styles: { ".MathJax_Display": { margin: 0 } },
+          linebreaks: { automatic: true }
+        },
+        SVG: {
+          scale: 100
         }
-        if (window.MathJax) {
-            window.MathJax.Hub.Queue(["Typeset", window.MathJax.Hub, "post"]);
-        }
-    })
-
-    function updatePost(postID) {
-        let db = firebase.firestore()
-        const postRef = db.collection('posts').doc(postID)
-        postRef.get().then(function(doc) {
-            if(doc.exists) {
-                post = doc.data()
-            }
-        })
+      });
     }
 
-    function date(timestamp) {
-        let date = new Date(timestamp * 1000);
-        let month = date.getMonth()
-        let months = [
-            'January',
-            'February',
-            'March',
-            'April',
-            'May',
-            'June',
-            'July',
-            'August',
-            'September',
-            'October',
-            'November',
-            'December'
-        ]
-        month = months[month]
-        date = `${month} ${date.getDate()}, ${date.getFullYear()}`
-        return date
+    updatePost(params.slug);
+  });
+
+  function renderArticle() {
+    if (post) {
+      document.getElementById("output").innerHTML = post.body;
+
+      if (window.MathJax) {
+        window.MathJax.Hub.Queue(["Typeset", window.MathJax.Hub, "output"]);
+      }
     }
+  }
+
+  function updatePost(postID) {
+    let db = firebase.firestore();
+    const postRef = db.collection("posts").doc(postID);
+    postRef.get().then(function(doc) {
+      if (doc.exists) {
+        post = doc.data();
+        renderArticle();
+      }
+    });
+  }
+
+  function date(timestamp) {
+    let date = new Date(timestamp * 1000);
+    let month = date.getMonth();
+    let months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December"
+    ];
+    month = months[month];
+    date = `${month} ${date.getDate()}, ${date.getFullYear()}`;
+    return date;
+  }
 </script>
 
 <style>
-    article {
-        width: 100%;
-        max-width: 50rem;
-        margin-bottom: 5rem;
-        padding: 0 1rem;
-    }
+  article {
+    width: 100%;
+    max-width: 50rem;
+    margin-bottom: 5rem;
+    padding: 0 1rem 5rem 0;
+  }
 
-    header {
-        display: flex;
-        flex-flow: column;
-        align-items: flex-start;
-        padding-bottom: 1rem;
-        margin-bottom: 1rem;
-        width: 100%;
-        border-bottom: 1px #d6d9dc solid;
-    }
+  header {
+    display: flex;
+    flex-flow: column;
+    align-items: flex-start;
+    padding-bottom: 1rem;
+    margin-bottom: 1rem;
+    width: 100%;
+    border-bottom: 1px #d6d9dc solid;
+  }
 
-    .date {
-        font-weight: 300;
-        color: #656462;
-    }
+  .date {
+    font-weight: 300;
+    color: #656462;
+  }
 
-    .video-container {
-        position: relative;
-        width: 100%;
-        padding-bottom: 56.25%;
-        height: 0;
-        margin-bottom: 2rem;
-    }
+  .video-container {
+    position: relative;
+    width: 100%;
+    padding-bottom: 56.25%;
+    height: 0;
+    margin-bottom: 2rem;
+  }
 
-    .video-container iframe {
-        position: absolute;
-        top: 0;
-        left: 0;
-        width: 100%;
-        height: 100%;
-    }
+  .video-container iframe {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+  }
 
-    .video-container-limiter {
-        width: 100%;
-        max-width: 80rem;
-    }
+  .video-container-limiter {
+    width: 100%;
+    max-width: 80rem;
+  }
 </style>
 
 {#if post}
-    <div class="video-container-limiter">
-        <div class="video-container">
-            <iframe src="{post.video}" frameborder="0" title="video" class="video"></iframe>
-        </div>
+  <div class="video-container-limiter">
+    <div class="video-container">
+      <iframe src={post.video} frameborder="0" title="video" class="video" />
     </div>
-    <article id="post">
-        <header>
-            <h1>{post.title}</h1>
-            <span class="date">{date(post.published.seconds)}</span>
-        </header>
+  </div>
+  <article id="post">
+    <header>
+      <h1>{post.title}</h1>
+      <span class="date">{date(post.published.seconds)}</span>
+    </header>
 
-        {@html post.body}
-    </article>
+    <div id="output" />
+  </article>
 {/if}
